a27cf0909d06be78b81c201e2f7d257299a2b415
Licheng Fang <andyfang1967@gmail.com>
2025-03-20 20:52:04 -0400

    Finished Day 15 part 2 using a deque

--- Day 13.py
+++ Day 13.py
@@ -2,10 +2,18 @@ import re
 
 file_name = "input"
 file = open(file_name)
-lines = file.readlines()
+lines = file.read()
 file.close()
 
-for line in lines:
-    ax, ay = map(int,re.findall(r"\d+", line))
-    print(ax, ay )
+total = 0
+for line in lines.split("\n\n"):
+    ax, ay, bx, by, px, py = map(int,re.findall(r"\d+", line))
+    px += 10000000000000
+    py += 10000000000000
+    ca = ((px *  by) - (py * bx)) / ((ax * by) - (ay*bx))
+    cb = (px - ax * ca) / bx
 
+    if ca % 1 == 0 and cb % 1 == 0:
+        total += ca * 3 + cb
+
+print(total)
