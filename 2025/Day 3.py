38eec1fa631f9df6bdf2aaf94db808e65dc1386d
LichengFang <andyfang1967@gmail.com>
2025-12-03 08:06:55 -0500

    Advent of Code day 3 part 1 did a for loop and a nested for loop to check all of the combinations.

--- 2025/Day 3.py
+++ 2025/Day 3.py
@@ -1,13 +1,19 @@
 file = open("../input").read().split("\n")
-max_num = 0
-total = 0
-for bank in file:
-    print(bank)
+
+
+def part_one():
+    total = 0
     max_num = 0
-    for i in range(len(bank)):
-        for j in range(i+1, len(bank)):
-            max_num = max(int(bank[i]+bank[j]),max_num)
+    for bank in file:
+        print(bank)
+        max_num = 0
+        for i in range(len(bank)):
+            for j in range(i+1, len(bank)):
+                max_num = max(int(bank[i]+bank[j]),max_num)
 
-    total += max_num
+        total += max_num
+    return total
 
-print(total)
+print(part_one())
+
+
