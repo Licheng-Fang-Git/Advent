493264a2081d9e93097af0c6bdf8f35b9623cb89
LichengFang <andyfang1967@gmail.com>
2025-12-03 14:03:12 -0500

    Finished Day 3 joltages part 2 was to keep on getting the max jolts and making sure to keep the length of the bank 12.

--- 2025/Day 3.py
+++ 2025/Day 3.py
@@ -1,6 +1,5 @@
 file = open("../input").read().split("\n")
 
-
 def part_one():
     total = 0
     max_num = 0
@@ -13,7 +12,36 @@ def part_one():
 
         total += max_num
     return total
+def part_two():
+    total = 0
+    for bank in file:
+        length = 12
+        battery = ""
+        prev_pos = 0
+        while length >= 1:
+            max_battery = 0
+            if battery == "":
+                start = 0
+            else:
+                start = prev_pos + 1
+                print(start, len(bank), )
+            for i in range(start, len(bank)):
+                if len(bank) - i < length:
+                    continue
+                if int(bank[i]) > max_battery and len(bank) - i >= length:
+                    print(i,bank[i], max_battery, len(bank) - i, length)
+                    max_battery = int(bank[i])
+                    prev_pos = i
 
-print(part_one())
+            battery += str(max_battery)
+            length -= 1
+            print(battery)
+        total += int(battery)
+
+    print(total)
+
+
+# print(part_one())
+part_two()
 
 
