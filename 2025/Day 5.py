7871d9a34ee71a9d0220ee6d083526da96d0144b
Licheng-Fang-Git <andyfang1967@gmail.com>
2025-12-05 18:16:06 -0500

    Advent Day 5 part two was harder than the rest of the dats so far. I needed to sort the list and then keep track of my ranges so far. If my last range is smaller than my current range then I just add the count of the smaller range into my total. I set the next range from the current range to it's maximum. Else I just set my last range to the last range with the first index being the max of the last range and current range. This helps make it inclusive.

---
+++ 2025/Day 5.py
@@ -0,0 +1,37 @@
+block1, block2 = open("../input").read().split('\n\n')
+def part_one():
+    count = 0
+    for ingredient in list(map(int, block2.split("\n"))):
+        for ingredient_ranges in block1.split("\n"):
+            start, end = map(int, ingredient_ranges.split("-"))
+            if start <= ingredient <= end:
+                count += 1
+                break
+    return count
+
+def part_two():
+    count = 0
+    all_ranges = sorted([ (int(ingredient_ranges.split("-")[0]), int(ingredient_ranges.split("-")[1])) for ingredient_ranges in block1.split("\n")])
+    last_range = None
+    for ingredient_range in all_ranges:
+        low, high = ingredient_range
+        if last_range is None:
+            last_range = ingredient_range
+        elif last_range[1] < low:
+            count += last_range[1] - last_range[0] + 1
+            last_range = (low, high)
+        else:
+            last_range = (last_range[0], max(last_range[1], high))
+    count += last_range[1] - last_range[0] + 1
+    return count
+
+print(part_two())
+
+
+
+
+
+
+
+part_two()
+
